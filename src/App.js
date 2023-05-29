2717a5f39b48a99e5c0ca60427e4b21e04fcb9a7
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-05-29 21:47:37 +0530

    local storage updates

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   const [activebar, setActiveBar] = useState("All");
   const [todo, setTodo] = useState("");
+  const [localItems, setLocalItems] = useState([]);
+
+  useEffect(() => {
+    const items = localStorage.getItem("to_do");
+    if (items) {
+      setLocalItems(JSON.parse(items));
+    }
+  }, []);
 
   const changeMenu = (e) => {
     setActiveBar(e.target.innerText);
   };
+  useEffect(() => {
+    localItems.length > 0 &&
+      localStorage.setItem("to_do", JSON.stringify(localItems));
+  }, [localItems]);
 
   const submitTodo = () => {
     const finalTodo = { active: true, to_do: todo };
-    const lsIt = JSON.parse(localStorage.getItem("to_do"));
-    var inLocal = [];
-    if (lsIt) {
-      inLocal = [...lsIt, finalTodo];
-    } else {
-      inLocal = [finalTodo];
-    }
-    localStorage.setItem("to_do", JSON.stringify(inLocal));
+    setLocalItems((prev) => [...prev, finalTodo]);
   };
   return (
     <main>
       <h1>#todo</h1>
       <Navbar activebar={activebar} changeMenu={changeMenu} />
       <div className="searchBar">
-        <input type="text" onChange={(e) => setTodo(e.target.value)} />
+        <input type="text" onBlur={(e) => setTodo(e.target.value)} />
         <button className="searchButton" onClick={submitTodo}>
           Add
         </button>
