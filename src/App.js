9ec47e26c9ee24f7c3e1e8002f6869f1a2ff9759
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-05-29 19:20:55 +0530

    active bar changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [activebar, setActiveBar] = useState("All");
+
+  const changeMenu = (e) => {
+    setActiveBar(e.target.innerText);
+  };
+  console.log(activebar);
   return (
     <main>
       <h1>#todo</h1>
       <nav>
-        <span className="active">All</span>
-        <span>Active</span>
-        <span>Completed</span>
+        <span className={activebar === "All" ? "active":""} onClick={changeMenu}>
+          All
+        </span>
+        <span className={activebar === "Active" ? "active":""}  onClick={changeMenu}>Active</span>
+        <span className={activebar === "Completed" ? "active":""}  onClick={changeMenu}>Completed</span>
       </nav>
     </main>
   );
