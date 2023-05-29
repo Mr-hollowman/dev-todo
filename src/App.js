a82d2440518f3008d73b58550ab698815b41a0d7
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-05-29 19:52:28 +0530

    todo added into local storage

--- src/App.js
+++ src/App.js
@@ -9,13 +9,27 @@ export default function App() {
   const changeMenu = (e) => {
     setActiveBar(e.target.innerText);
   };
+
+  const submitTodo = () => {
+    const finalTodo = { active: true, to_do: todo };
+    const lsIt = JSON.parse(localStorage.getItem("to_do"));
+    var inLocal = [];
+    if (lsIt) {
+      inLocal = [...lsIt, finalTodo];
+    } else {
+      inLocal = [finalTodo];
+    }
+    localStorage.setItem("to_do", JSON.stringify(inLocal));
+  };
   return (
     <main>
       <h1>#todo</h1>
       <Navbar activebar={activebar} changeMenu={changeMenu} />
       <div className="searchBar">
         <input type="text" onChange={(e) => setTodo(e.target.value)} />
-        <button className="searchButton">Add</button>
+        <button className="searchButton" onClick={submitTodo}>
+          Add
+        </button>
       </div>
     </main>
   );
