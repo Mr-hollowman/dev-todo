1f999a1d8172f3456a6a89d0a2cb92f8f8c29b7e
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-05-29 23:10:12 +0530

    changes update

--- src/App.js
+++ src/App.js
@@ -23,8 +23,25 @@ export default function App() {
   }, [localItems]);
 
   const submitTodo = () => {
-    const finalTodo = { active: true, to_do: todo };
+    const finalTodo = { id: localItems.length, active: true, to_do: todo };
     setLocalItems((prev) => [...prev, finalTodo]);
+  };
+
+  const handleChange = (id, bool) => {
+    console.log(id, "id");
+    setLocalItems((prev) => {
+      var changedEle = prev.map((item) => {
+        if (item.id == id) {
+          item.active = bool;
+          console.log(item, "after change");
+          return item;
+        } else {
+          return item;
+        }
+      });
+      console.log(changedEle, "before return");
+      return [...changedEle];
+    });
   };
   return (
     <main>
@@ -35,6 +52,20 @@ export default function App() {
         <button className="searchButton" onClick={submitTodo}>
           Add
         </button>
+      </div>
+      <div className="todo_items">
+        {localItems.map((item) => {
+          return (
+            <div key={item.id} className="todo_list">
+              <input
+                type="checkbox"
+                checked={item.active === false}
+                onChange={() => handleChange(item.id, item.active === false)}
+              />
+              {item.active ? <span>{item.to_do}</span> : <s>{item.to_do}</s>}
+            </div>
+          );
+        })}
       </div>
     </main>
   );
